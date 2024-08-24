21b807cca12abc1a30c38da3a9ea1e86f4692bb3
Gustavo Santos <devgustavosantos@gmail.com>
2024-08-24 13:10:15 -0300

    feat: adds styling for the Wrapper

--- src/components/Wrapper/Wrapper.styles.ts
+++ src/components/Wrapper/Wrapper.styles.ts
@@ -1,7 +1,13 @@
 import { styled } from 'styled-components';
 
+import { BREAKPOINTS } from '@/styles';
+
 export const WrapperContainer = styled.div`
   max-width: 1152px;
   padding-inline: 16rem;
   margin: auto;
+
+  @media only screen and (max-width: ${BREAKPOINTS.MEDIUM}) {
+    padding-inline: 12rem;
+  }
 `;
