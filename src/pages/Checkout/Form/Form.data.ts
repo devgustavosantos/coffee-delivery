0ca120b055da3a97e0379781c9de3af25741d68b
Gustavo Santos <devgustavosantos@gmail.com>
2024-10-17 08:24:06 -0300

    feat: wip2 - collect order information
    improves type definition

--- src/pages/Checkout/Form/Form.data.ts
+++ src/pages/Checkout/Form/Form.data.ts
@@ -1,15 +1,27 @@
 import { z } from 'zod';
 
-const addressInfos = [
-  { name: 'postalCode' as const, placeholder: 'CEP' },
-  { name: 'street' as const, placeholder: 'Rua' },
-  { name: 'number' as const, placeholder: 'Número', type: 'number' },
-  { name: 'complement' as const, placeholder: 'Complemento', isOptional: true },
-  { name: 'neighborhood' as const, placeholder: 'Bairro' },
-  { name: 'city' as const, placeholder: 'Cidade' },
+import { AddressInfo, State } from './Form.types';
+
+const AddressSchema = z.object({
+  postalCode: z.string().length(9),
+  street: z.string().min(3).max(255),
+  number: z.number().int().min(0).max(99999),
+  complement: z.string().min(1).max(255).optional(),
+  neighborhood: z.string().min(2).max(255),
+  city: z.string().min(2).max(255),
+  state: z.string().length(2),
+});
+
+const addressInfos: AddressInfo[] = [
+  { name: 'postalCode', placeholder: 'CEP' },
+  { name: 'street', placeholder: 'Rua' },
+  { name: 'number', placeholder: 'Número', type: 'number' },
+  { name: 'complement', placeholder: 'Complemento', isOptional: true },
+  { name: 'neighborhood', placeholder: 'Bairro' },
+  { name: 'city', placeholder: 'Cidade' },
 ];
 
-const states = [
+const states: State[] = [
   { value: 'UF', disabled: true, selected: true },
   { value: 'AC' },
   { value: 'AL' },
@@ -39,15 +51,5 @@ const states = [
   { value: 'SP' },
   { value: 'TO' },
 ];
-
-const AddressSchema = z.object({
-  postalCode: z.string().length(9),
-  street: z.string().min(3).max(255),
-  number: z.number().int().min(0).max(99999),
-  complement: z.string().min(1).max(255).optional(),
-  neighborhood: z.string().min(2).max(255),
-  city: z.string().min(2).max(255),
-  state: z.string().length(2),
-});
 
 export { addressInfos, states, AddressSchema };
--- src/pages/Checkout/Form/Form.types.ts
+++ src/pages/Checkout/Form/Form.types.ts
@@ -4,4 +4,45 @@ import { AddressSchema } from './Form.data';
 
 type AddressType = z.infer<typeof AddressSchema>;
 
-export type { AddressType };
+interface AddressInfo {
+  name: keyof AddressType;
+  placeholder: string;
+  type?: 'number';
+  isOptional?: true;
+}
+
+interface State {
+  value:
+    | 'UF'
+    | 'AC'
+    | 'AL'
+    | 'AM'
+    | 'AP'
+    | 'BA'
+    | 'CE'
+    | 'DF'
+    | 'ES'
+    | 'GO'
+    | 'MA'
+    | 'MG'
+    | 'MS'
+    | 'MT'
+    | 'PA'
+    | 'PB'
+    | 'PE'
+    | 'PI'
+    | 'PR'
+    | 'RJ'
+    | 'RN'
+    | 'RO'
+    | 'RR'
+    | 'RS'
+    | 'SC'
+    | 'SE'
+    | 'SP'
+    | 'TO';
+  disabled?: true;
+  selected?: true;
+}
+
+export type { AddressInfo, AddressType, State };
