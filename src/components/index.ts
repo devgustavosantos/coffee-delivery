4d05638f126a2e62bf2db3473c298c68b94a3852
Gustavo Santos <devgustavosantos@gmail.com>
2024-10-23 06:29:13 -0300

    feat(components): creates the Error

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
 import { Badge } from './Badge';
+import { Error } from './Error';
 import { Footer } from './Footer';
 import { Header } from './Header';
 import { Product } from './Product';
 
-export { Badge, Footer, Header, Product };
+export { Badge, Footer, Header, Product, Error };
